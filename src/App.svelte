e00dfd43aa0dc528db18d987ceb2e556bde80825
Eric Xiao <erixrekt69@gmail.com>
2020-03-18 14:04:54 -0400

    Styling for news editor

--- src/App.svelte
+++ src/App.svelte
@@ -2,16 +2,45 @@
 	import { onMount } from 'svelte';
 	import SimpleMDE from "SimpleMDE";
 	let editorElement;
+	let simplemde;
 
 	onMount(() => {
-		const simplemde = new SimpleMDE({ element: editorElement });
+		simplemde = new SimpleMDE({
+			autosave: {
+				enabled: true,
+				uniqueId: "MyUniqueID",
+				delay: 1000,
+			},
+			placeholder: "Press F9 to enter and exit side-by-side with live preview. Press F11 to enter and exit fullscreen mode.",
+			hideIcons: ["preview"],
+			element: editorElement
+		});
 	});
+
+	function reset() {
+		simplemde.value("");
+	}
 </script>
 
 <main>
 	<h1>Hello there!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-	<textarea bind:this={editorElement}></textarea>
+	<div class="editor">
+		<div class="header">
+			<h1>News Editor</h1>
+		</div>
+
+		<div class="ui grid">
+			<div class="sixteen wide column">
+				<textarea bind:this={editorElement}></textarea>
+			</div>
+		</div>
+
+		<div class="footer">
+			<button class="save">Save</button>
+			<button class="cancel" on:click={reset}>Cancel</button>
+		</div>
+	</div>
 </main>
 
 <style>
@@ -19,6 +48,7 @@
 		padding: 1em;
 		max-width: 240px;
 		margin: 0 auto;
+		width: 100%;
 	}
 
 	h1 {
@@ -34,7 +64,40 @@
 		}
 	}
 
-	#newsEditor {
-		margin-top: 10px;
+	.editor {
+		width: 90%;
+		margin-left: 5%;
+		display: flex;
+		flex-direction: column;
+	}
+
+	.header {
+		background-color: rgb(92, 92, 252);
+		padding: 20px;
+	}
+
+	.ui {
+		flex-grow: 2;
+	}
+
+	.footer {
+		background-color: rgb(92, 92, 252);
+		display: flex;
+		flex-direction: row-reverse;
+		padding: 20px;
+	}
+
+	button {
+		margin-right: 30px;
+		border: none;
+		padding: 10px;
+	}
+
+	.save {
+		background-color: yellow;
+	}
+
+	.cancel {
+		background-color: orange;
 	}
 </style>
